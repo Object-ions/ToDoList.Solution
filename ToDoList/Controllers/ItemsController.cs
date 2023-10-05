32b60ee59781915265367650162ccbb2e904aad9
object-ions <moshikoatia@gmail.com>
2023-10-04 23:53:19 -0400

    modify files to follow LHTP

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -16,12 +18,15 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items
+                            .Include(item => item.Category)
+                            .ToList();
+      ViewBag.PageTitle = "View All Items";
       return View(model);
     }
-
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
@@ -31,6 +36,13 @@ namespace ToDoList.Controllers
       _db.Items.Add(item);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
     }
   }
 }
