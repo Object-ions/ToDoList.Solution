43c677d4b7f57f9e344dcf6d70ad3d7b70c48cfb
object-ions <moshikoatia@gmail.com>
2023-10-05 11:25:02 -0400

    WIP. following LHTP

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -33,6 +33,10 @@ namespace ToDoList.Controllers
     [HttpPost]
     public ActionResult Create(Item item)
     {
+      if (item.CategoryId == 0)
+      {
+        return RedirectToAction("Create");
+      }
       _db.Items.Add(item);
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -42,6 +46,14 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items
+                          .Include(item => item.Category)
+                          .FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
   }
